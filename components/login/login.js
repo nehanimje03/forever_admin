182a8c440d92046c3e291573f36d28387a1ee943
manishaN03 <manishanimje03@gmail.com>
2026-05-20 23:03:55 +0530

    change in login

--- components/login/login.js
+++ components/login/login.js
@@ -28,6 +28,7 @@ const Login = () => {
             router.push("/");
           }
         },
+        
         onError: (error) => {
           console.error("Login failed:", error.message);
         },
